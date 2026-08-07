5ca7f497657cabd7bd3f06c5019d0ca542e6a847
Washim Akhtar <120321773+washimakhtar123@users.noreply.github.com>
2026-08-07 12:42:50 +0530

    Time: 1 ms (99.96%), Space: 77.3 MB (49.36%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -12,3 +12,19 @@ class Solution {
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int minubArray(int[] nums) {
+//         int bestending=nums[0];
+//         int ans=nums[0];
+
+//         for(int i = 1; i < nums.length; i++){
+//             int c1=bestending+nums[i];
+//             int c2=nums[i];
+//             bestending=Math.min(c1,c2);
+//             ans=Math.min(ans,bestending);
+//         }
+//         return ans;
+//     }
+// }
