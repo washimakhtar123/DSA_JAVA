c35fa6396a8584e5db25c7256f161aa03379a52a
Washim Akhtar <120321773+washimakhtar123@users.noreply.github.com>
2026-08-09 19:38:29 +0530

    Time: 1 ms (97.51%), Space: 47.3 MB (60.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,33 +1,57 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        int left = 0;
+        int Sum = 0;
 
-        int prefix[] = new int[nums.length];
-        int suffix[] = new int[nums.length];
-
-        // Fill the prefix array
-        // prefix[i] = i ke LEFT side ka total sum
-        prefix[0] = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
-        }
-
-        // Fill the suffix array
-        // suffix[i] = i ke RIGHT side ka total sum
-        suffix[nums.length - 1] = 0;
-
-        for (int i = nums.length - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + nums[i + 1];
-        }
-
-        // Compare prefix and suffix at SAME index
         for (int i = 0; i < nums.length; i++) {
+            Sum += nums[i];
+        }
 
-            if (prefix[i] == suffix[i]) {
+        for (int i = 0; i < nums.length; i++) {
+            int right = Sum - left - nums[i];
+
+            if (left == right) {
                 return i;
             }
+
+            left += nums[i];
         }
 
         return -1;
     }
 }
+
+
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+
+//         int prefix[] = new int[nums.length];
+//         int suffix[] = new int[nums.length];
+
+//         // Fill the prefix array
+//         // prefix[i] = i ke LEFT side ka total sum
+//         prefix[0] = 0;
+
+//         for (int i = 1; i < nums.length; i++) {
+//             prefix[i] = prefix[i - 1] + nums[i - 1];
+//         }
+
+//         // Fill the suffix array
+//         // suffix[i] = i ke RIGHT side ka total sum
+//         suffix[nums.length - 1] = 0;
+
+//         for (int i = nums.length - 2; i >= 0; i--) {
+//             suffix[i] = suffix[i + 1] + nums[i + 1];
+//         }
+
+//         // Compare prefix and suffix at SAME index
+//         for (int i = 0; i < nums.length; i++) {
+
+//             if (prefix[i] == suffix[i]) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
