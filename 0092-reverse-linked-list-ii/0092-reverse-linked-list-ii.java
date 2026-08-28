112554e7b6b316c3828377b79d7b6a51a1854cdd
Washim Akhtar <120321773+washimakhtar123@users.noreply.github.com>
2026-08-28 16:16:52 +0530

    Time: 0 ms (100%), Space: 42.3 MB (99.51%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -7,42 +7,65 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
-
-
- class Solution {
+ */class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
 
-        // Dummy node helps when left = 1
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        // Move prev to the node just before 'left'
-        ListNode prev = dummy;
-
-        for (int i = 1; i < left; i++) {
-            prev = prev.next;
+        // If list is empty or nothing needs to be reversed
+        if (head == null || left == right) {
+            return head;
         }
 
-        // curr points to the first node that we need to reverse
-        ListNode curr = prev.next;
+        // 'before' will point to the node before left
+        ListNode before = null;
 
-        // Reverse nodes one by one
-        for (int i = 0; i < right - left; i++) {
+        // curr will reach the left position
+        ListNode curr = head;
 
-            // Node that will be moved to the front
+        int pos = 1;
+
+        // Move curr to the 'left' position
+        while (pos < left) {
+            before = curr;
+            curr = curr.next;
+            pos++;
+        }
+
+        // Save the first node of the reversing part
+        ListNode first = curr;
+
+        // Normal linked-list reversal
+        ListNode prev = null;
+
+        while (pos <= right) {
+
+            // Save next node
             ListNode next = curr.next;
 
-            // Remove 'next' from its current position
-            curr.next = next.next;
+            // Reverse the link
+            curr.next = prev;
 
-            // Put 'next' before curr
-            next.next = prev.next;
+            // Move prev forward
+            prev = curr;
 
-            // Connect prev to the moved node
-            prev.next = next;
+            // Move curr forward
+            curr = next;
+
+            pos++;
         }
 
-        return dummy.next;
+        // Connect the node before 'left'
+        // with the new first node of reversed part
+        if (before != null) {
+            before.next = prev;
+        } else {
+            // If left == 1, prev becomes the new head
+            head = prev;
+        }
+
+        // 'first' is now the last node of reversed part
+        // Connect it with the remaining list
+        first.next = curr;
+
+        return head;
     }
 }
